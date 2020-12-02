399081420aeae6405b3d5ed2c21e443fad1f2d12
viniciuspenha <penhavinicius@hotmail.com>
2020-12-02 17:25:19 -0300

    spring security

--- src/main/java/br/com/viniciuspenha/gerenciadorpagamento/GerenciadorPagamentoApplication.java
+++ src/main/java/br/com/viniciuspenha/gerenciadorpagamento/GerenciadorPagamentoApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableResourceServer
 public class GerenciadorPagamentoApplication {
 
     @Bean
